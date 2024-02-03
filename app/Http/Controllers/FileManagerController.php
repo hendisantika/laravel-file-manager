ed04c84dea85a476917b69e78ee177f01ad84648
hendisantika <hendisantika@yahoo.co.id>
2024-02-04 06:01:15 +0700

    Add index in FileManagerController 🌀✏️📗🐧🐳

--- app/Http/Controllers/FileManagerController.php
+++ app/Http/Controllers/FileManagerController.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Response;
+
 class FileManagerController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return view('filemanager');
+    }
 }
